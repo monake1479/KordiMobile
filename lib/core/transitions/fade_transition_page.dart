7b7c5af1623f082d7019e73bb2bce91912b4c76b
Daniel <daniel.oblak99@gmail.com>
2023-09-29 19:34:40 +0200

    Implemented FadeTransitionPage

---
+++ lib/core/transitions/fade_transition_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class FadeTransitionPage extends CustomTransitionPage<void> {
+  FadeTransitionPage({
+    required LocalKey super.key,
+    required super.child,
+  }) : super(
+          transitionsBuilder: (c, animation, a2, child) => FadeTransition(
+            opacity: animation.drive(_curveTween),
+            child: child,
+          ),
+        );
+
+  static final _curveTween = CurveTween(curve: Curves.easeIn);
+}
