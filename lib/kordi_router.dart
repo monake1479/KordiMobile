1cd2e0b87f20f7c9b855f4cd1aef722329cd9de1
Daniel <daniel.oblak99@gmail.com>
2023-09-29 19:33:53 +0200

    Changed how KordiRouter contains routes for pages

--- lib/kordi_router.dart
+++ lib/kordi_router.dart
@@ -2,36 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:kordi_mobile/home_page.dart';
 
+part 'kordi_router.g.dart';
+
 class KordiRouter {
   static GoRouter router(BuildContext context) {
     return GoRouter(
       initialLocation: '/',
       debugLogDiagnostics: true,
-      routes: [
-        GoRoute(path: '/', builder: (context, state) => HomePage()),
-      ]
-      // errorPageBuilder: (context, state) {
-      //   log(state.error.toString());
-      //   return MaterialPage<void>(
-      //     key: state.pageKey,
-      //     child: const ErrorPage(),
-      //   );
-      // },
-      ,
+      routes: $appRoutes,
     );
   }
-}
-
-class FadeTransitionPage extends CustomTransitionPage<void> {
-  FadeTransitionPage({
-    required LocalKey super.key,
-    required super.child,
-  }) : super(
-          transitionsBuilder: (c, animation, a2, child) => FadeTransition(
-            opacity: animation.drive(_curveTween),
-            child: child,
-          ),
-        );
-
-  static final _curveTween = CurveTween(curve: Curves.easeIn);
 }
