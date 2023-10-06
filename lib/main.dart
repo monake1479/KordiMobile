807a4bca0fa4fc45a97cbb3ef8a765f0d4f7791f
Daniel <daniel.oblak99@gmail.com>
2023-10-06 23:10:09 +0200

    Initialization of FlutterNativeSplash in main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:kordi_mobile/app.dart';
 import 'package:kordi_mobile/dependency_injection.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+  final WidgetsBinding widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
   );
