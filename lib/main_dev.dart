cd851e6805eb04e4679c7e7ae8db52bc5cebc128
Daniel Obłąk <daniel.oblak@codeclusive.io>
2023-10-12 09:09:36 +0200

    Changed how app is launch in main and main_dev files

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -2,5 +2,5 @@ import 'package:kordi_mobile/bootstrap.dart';
 import 'package:kordi_mobile/kordi_environment.dart';
 
 void main() {
-  bootstrap(env: KordiEnvironmentProd());
+  bootstrap(env: KordiEnvironmentDev());
 }
