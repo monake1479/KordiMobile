afc0a3c9ae497e4749670456ba7a600102e76d89
Daniel Obłąk <daniel.oblak@codeclusive.io>
2023-10-15 16:34:09 +0200

    Implemented signUp method in SignUpService

--- lib/sign_up/services/sign_up_service.dart
+++ lib/sign_up/services/sign_up_service.dart
@@ -1,14 +1,38 @@
+import 'dart:developer';
+
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:kordi_mobile/core/models/kordi_exception.dart';
+import 'package:kordi_mobile/core/utils/dio_client.dart';
+import 'package:kordi_mobile/dependency_injection.dart';
 import 'package:kordi_mobile/sign_up/interfaces/sign_up_interface.dart';
 import 'package:kordi_mobile/sign_up/models/sign_up_dto.dart';
+import 'package:kordi_mobile/sign_up/utils/response_code_converter.dart';
 
-@singleton
+@Singleton(as: SignUpInterface)
 class SignUpService implements SignUpInterface {
   @override
   Future<Either<KordiException, Unit>> signUp(SignUpDto dto) async {
-    // TODO: implement signUp
-    throw UnimplementedError();
+    late Either<KordiException, Unit> result;
+    final dioClient = getIt.get<DioClient>();
+    try {
+      await dioClient.dio.post('/register', data: dto.toJson());
+      result = right(unit);
+    } on DioException catch (e, s) {
+      log('[SignUpService] signUp() exception: $e', stackTrace: s);
+      if (e.response?.statusCode == 401) {
+        result = left(KordiException.unauthorized());
+      } else if (e.response?.statusCode == 400) {
+        final String message =
+            ResponseCodeConverter.convert(e.response?.data['error']);
+        result = left(
+          KordiException.customMessage(message: message),
+        );
+      } else {
+        result = left(KordiException.serverError());
+      }
+    }
+    return result;
   }
 }
