084b87120517f034847f4a666ecf39044e1ff607
Daniel Obłąk <daniel.oblak@codeclusive.io>
2023-10-21 12:05:36 +0200

    Added import for $appRoutes param in KordiRouter

--- lib/core/utils/kordi_router.dart
+++ lib/core/utils/kordi_router.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:kordi_mobile/collections/pages/collection_page.dart';
 import 'package:kordi_mobile/core/pages/kordi_scaffold.dart';
+import 'package:kordi_mobile/core/utils/kordi_routes.dart';
 
 class KordiRouter {
   static GoRouter router(BuildContext context) {
