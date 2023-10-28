5fcb633a64dc1101ed5f5a092a205a71f8da2fad
Daniel <daniel.oblak99@gmail.com>
2023-10-28 09:54:06 +0200

    Implemented verifyByPhone and verifyByEmail methods in VerificationCodeService

--- lib/verification_code/services/verification_code_service.dart
+++ lib/verification_code/services/verification_code_service.dart
@@ -37,16 +37,46 @@ class VerificationCodeService implements VerificationCodeInterface {
   }
 
   @override
-  Future<Either<KordiException, Unit>> verify(
-      String username, String code) async {
-    log('[VerificationCodeService] resend()');
+  Future<Either<KordiException, Unit>> verifyByEmail(
+    String code,
+  ) async {
+    log('[VerificationCodeService] verifyByEmail()');
     final DioClient dioClient = getIt.get<DioClient>();
     late Either<KordiException, Unit> result;
     try {
-      await dioClient.dio.post('/verifyToken?token=$code');
+      await dioClient.dio.post('/verify?token=$code');
       result = right(unit);
     } on DioException catch (e, s) {
-      log('[VerificationCodeService] resend() DioException: $e, $s');
+      log('[VerificationCodeService] verifyByEmail() DioException: $e, $s');
+
+      if (e.response?.statusCode == 401) {
+        result = left(KordiException.unauthorized());
+      } else if (e.response?.statusCode == 400) {
+        final String message =
+            ResponseCodeConverter.convert(e.response?.data['error']);
+        result = left(
+          KordiException.customMessage(message: message),
+        );
+      } else {
+        result = left(KordiException.serverError());
+      }
+    }
+    return result;
+  }
+
+  @override
+  Future<Either<KordiException, Unit>> verifyByPhone(
+    String phoneNumber,
+    String code,
+  ) async {
+    log('[VerificationCodeService] verifyByPhone()');
+    final DioClient dioClient = getIt.get<DioClient>();
+    late Either<KordiException, Unit> result;
+    try {
+      await dioClient.dio.post('/verify?phone=$phoneNumber&token=$code');
+      result = right(unit);
+    } on DioException catch (e, s) {
+      log('[VerificationCodeService] verifyByPhone() DioException: $e, $s');
 
       if (e.response?.statusCode == 401) {
         result = left(KordiException.unauthorized());
