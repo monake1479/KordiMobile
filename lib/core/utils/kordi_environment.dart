28f7c259bb1cbe2c6b8e29008f124a73d998d4b4
Daniel <daniel.oblak99@gmail.com>
2023-10-28 09:45:56 +0200

    Implemented diversity for localhost base url for emulators

--- lib/core/utils/kordi_environment.dart
+++ lib/core/utils/kordi_environment.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 abstract class KordiEnvironment {
   KordiEnvironment._(this.baseUrl);
   final String baseUrl;
@@ -5,10 +7,22 @@ abstract class KordiEnvironment {
 
 class KordiEnvironmentDev implements KordiEnvironment {
   @override
-  String get baseUrl => 'http://10.0.2.2:8081';
+  String get baseUrl {
+    if (Platform.isAndroid) {
+      return 'http://10.0.2.2:8081';
+    } else {
+      return 'http://192.168.8.199:8081';
+    }
+  }
 }
 
 class KordiEnvironmentProd implements KordiEnvironment {
   @override
-  String get baseUrl => 'http://localhost:8081';
+  String get baseUrl {
+    if (Platform.isAndroid) {
+      return 'http://10.0.2.2:8081';
+    } else {
+      return 'http://192.168.8.199:8081';
+    }
+  }
 }
