582e49bfbb1cc59f38389ff3b555d0abbdbbf6ab
Daniel <daniel.oblak99@gmail.com>
2023-10-28 09:56:18 +0200

    Implemented VerificationCodeForm feature

---
+++ lib/verification_code/controllers/verification_code_form/verification_code_form_bloc.dart
@@ -0,0 +1,94 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+import 'package:kordi_mobile/verification_code/controllers/verification_code_controllers.dart';
+
+part 'verification_code_form_event.dart';
+part 'verification_code_form_state.dart';
+part 'verification_code_form_bloc.freezed.dart';
+
+@Injectable()
+class VerificationCodeFormBloc
+    extends Bloc<VerificationCodeFormEvent, VerificationCodeFormState> {
+  VerificationCodeFormBloc(
+    this._verificationCodeBloc,
+  ) : super(VerificationCodeFormState.initial()) {
+    on<_UpdatePhoneNumber>(_updatePhoneNumber);
+    on<_UpdateVerificationCode>(_updateVerificationCode);
+    on<_UpdateUsername>(_updateUsername);
+    on<_ValidateFields>(_validateFields);
+    on<_VerifyByEmail>(_verifyByEmail);
+    on<_VerifyByPhone>(_verifyByPhone);
+    on<_Resend>(_resend);
+  }
+  final VerificationCodeBloc _verificationCodeBloc;
+  void _updatePhoneNumber(
+    _UpdatePhoneNumber event,
+    Emitter<VerificationCodeFormState> emit,
+  ) {
+    emit(state.copyWith(phoneNumber: event.phoneNumber));
+  }
+
+  void _updateVerificationCode(
+    _UpdateVerificationCode event,
+    Emitter<VerificationCodeFormState> emit,
+  ) {
+    emit(state.copyWith(verificationCode: event.verificationCode));
+  }
+
+  void _updateUsername(
+    _UpdateUsername event,
+    Emitter<VerificationCodeFormState> emit,
+  ) {
+    emit(state.copyWith(username: event.username));
+  }
+
+  void _validateFields(
+    _ValidateFields event,
+    Emitter<VerificationCodeFormState> emit,
+  ) {
+    if (!state.isCodeValid) {
+      emit(state.copyWith(verificationCode: ''));
+    }
+    if (!state.isPhoneNumberValid) {
+      emit(state.copyWith(phoneNumber: ''));
+    }
+    if (!state.isUsernameValid) {
+      emit(state.copyWith(username: ''));
+    }
+  }
+
+  void _verifyByEmail(
+    _VerifyByEmail event,
+    Emitter<VerificationCodeFormState> emit,
+  ) {
+    _verificationCodeBloc.add(
+      VerificationCodeEvent.verifyByEmail(
+        state.verificationCode!,
+      ),
+    );
+  }
+
+  void _verifyByPhone(
+    _VerifyByPhone event,
+    Emitter<VerificationCodeFormState> emit,
+  ) {
+    _verificationCodeBloc.add(
+      VerificationCodeEvent.verifyByPhone(
+        state.verificationCode!,
+        state.phoneNumber!,
+      ),
+    );
+  }
+
+  void _resend(
+    _Resend event,
+    Emitter<VerificationCodeFormState> emit,
+  ) {
+    _verificationCodeBloc.add(
+      VerificationCodeEvent.resend(
+        state.username!,
+      ),
+    );
+  }
+}
---
+++ lib/verification_code/controllers/verification_code_form/verification_code_form_state.dart
@@ -0,0 +1,35 @@
+part of 'verification_code_form_bloc.dart';
+
+@freezed
+class VerificationCodeFormState with _$VerificationCodeFormState {
+  const factory VerificationCodeFormState({
+    required bool isLoading,
+    required String? phoneNumber,
+    required String? verificationCode,
+    required String? username,
+  }) = _VerificationCodeFormState;
+
+  factory VerificationCodeFormState.initial() => VerificationCodeFormState(
+        isLoading: false,
+        phoneNumber: null,
+        verificationCode: null,
+        username: null,
+      );
+}
+
+extension VerificationCodeFormStateX on VerificationCodeFormState {
+  bool get showUsernameError => username != null && username!.isEmpty;
+  bool get isUsernameValid => username != null && username!.isNotEmpty;
+  bool get showPhoneNumberError => phoneNumber != null && phoneNumber!.isEmpty;
+  bool get isPhoneNumberValid =>
+      phoneNumber != null &&
+      phoneNumber!.isNotEmpty &&
+      phoneNumber!.length == 9;
+  bool get showCodeError =>
+      verificationCode != null && verificationCode!.isEmpty;
+  bool get isCodeValid =>
+      verificationCode != null &&
+      verificationCode!.isNotEmpty &&
+      verificationCode!.length == 6;
+  bool get isFormValid => isPhoneNumberValid && isCodeValid;
+}
