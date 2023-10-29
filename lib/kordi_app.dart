f3900034a6aeb31dc1214c685bfd5e7d0aa0ef34
Daniel <daniel.oblak99@gmail.com>
2023-10-29 21:58:42 +0100

    Added BlocListener on GetUserCubit in KordiApp

--- lib/kordi_app.dart
+++ lib/kordi_app.dart
@@ -5,9 +5,11 @@ import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:kordi_mobile/core/controllers/localization_bloc.dart';
 import 'package:kordi_mobile/core/utils/custom_scroll_behavior.dart';
+import 'package:kordi_mobile/core/utils/kordi_dialog.dart';
 import 'package:kordi_mobile/core/utils/kordi_router.dart';
 import 'package:kordi_mobile/dependency_injection.dart';
 import 'package:kordi_mobile/theme/color_schemes.dart';
+import 'package:kordi_mobile/user/controllers/get_user_cubit.dart';
 
 class KordiApp extends StatelessWidget {
   const KordiApp({super.key});
@@ -19,29 +21,39 @@ class KordiApp extends StatelessWidget {
       create: (context) => getIt.get<LocalizationBloc>(),
       child: BlocBuilder<LocalizationBloc, Locale>(
         builder: (context, state) {
-          return MaterialApp.router(
-            localizationsDelegates: AppLocalizations.localizationsDelegates,
-            supportedLocales: AppLocalizations.supportedLocales,
-            locale: state,
-            builder: (context, child) {
-              return ScrollConfiguration(
-                behavior: CustomScrollBehavior(),
-                child: child!,
-              );
+          return BlocListener<GetUserCubit, GetUserState>(
+            listener: (context, state) async {
+              if (state.exception != null) {
+                await KordiDialog.showException(
+                  context,
+                  state.exception!,
+                );
+              }
             },
-            theme: ThemeData(
-              useMaterial3: true,
-              colorScheme: lightColorScheme,
-              textTheme: GoogleFonts.barlowTextTheme(),
+            child: MaterialApp.router(
+              localizationsDelegates: AppLocalizations.localizationsDelegates,
+              supportedLocales: AppLocalizations.supportedLocales,
+              locale: state,
+              builder: (context, child) {
+                return ScrollConfiguration(
+                  behavior: CustomScrollBehavior(),
+                  child: child!,
+                );
+              },
+              theme: ThemeData(
+                useMaterial3: true,
+                colorScheme: lightColorScheme,
+                textTheme: GoogleFonts.barlowTextTheme(),
+              ),
+              darkTheme: ThemeData(
+                useMaterial3: true,
+                colorScheme: darkColorScheme,
+                textTheme: GoogleFonts.barlowTextTheme(),
+              ),
+              title: 'Kordi Mobile',
+              debugShowCheckedModeBanner: false,
+              routerConfig: KordiRouter.router(context),
             ),
-            darkTheme: ThemeData(
-              useMaterial3: true,
-              colorScheme: darkColorScheme,
-              textTheme: GoogleFonts.barlowTextTheme(),
-            ),
-            title: 'Kordi Mobile',
-            debugShowCheckedModeBanner: false,
-            routerConfig: KordiRouter.router(context),
           );
         },
       ),
