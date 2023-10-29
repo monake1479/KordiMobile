531ab303948ceb1ced7d86885a866a1b50d6b866
Daniel Obłąk <daniel.oblak@codeclusive.io>
2023-10-29 14:19:46 +0100

    Removed unused AuthEvent class

--- lib/auth/controllers/auth_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'auth_bloc.dart';
-
-@freezed
-class AuthEvent with _$AuthEvent {
-  const factory AuthEvent.init() = _Init;
-  const factory AuthEvent.login(String username, String password) = _Login;
-  const factory AuthEvent.reset() = _Reset;
-}
