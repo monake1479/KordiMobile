303d029eb49a9ae7d305390d3ed0fe976ab2a9f3
Daniel Obłąk <daniel.oblak@codeclusive.io>
2023-10-29 14:50:42 +0100

    Changed injectable decorator to LazySingleton above VerificationCodeBloc, added override close method in VerificationCodeBloc

--- lib/verification_code/controllers/verification_code/verification_code_bloc.dart
+++ lib/verification_code/controllers/verification_code/verification_code_bloc.dart
@@ -1,16 +1,19 @@
+import 'dart:developer';
+
 import 'package:dartz/dartz.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:kordi_mobile/core/models/kordi_exception.dart';
 import 'package:kordi_mobile/core/utils/either_extension.dart';
+import 'package:kordi_mobile/dependency_injection.dart';
 import 'package:kordi_mobile/verification_code/interfaces/verification_code_interface.dart';
 
 part 'verification_code_state.dart';
 part 'verification_code_event.dart';
 part 'verification_code_bloc.freezed.dart';
 
-@Injectable()
+@LazySingleton()
 class VerificationCodeBloc
     extends Bloc<VerificationCodeEvent, VerificationCodeState> {
   VerificationCodeBloc(this._service) : super(VerificationCodeState.initial()) {
@@ -24,7 +27,7 @@ class VerificationCodeBloc
     _VerifyByEmail event,
     Emitter<VerificationCodeState> emit,
   ) async {
-    emit(state.copyWith(isLoading: true));
+    emit(state.copyWith(isLoading: true, failureOrSuccessOption: none()));
     final response = await _service.verifyByEmail(event.code);
     if (response.isLeft()) {
       emit(
@@ -47,7 +50,7 @@ class VerificationCodeBloc
     _VerifyByPhone event,
     Emitter<VerificationCodeState> emit,
   ) async {
-    emit(state.copyWith(isLoading: true));
+    emit(state.copyWith(isLoading: true, failureOrSuccessOption: none()));
     final response = await _service.verifyByPhone(
       event.phoneNumber,
       event.code,
@@ -73,7 +76,7 @@ class VerificationCodeBloc
     _Resend event,
     Emitter<VerificationCodeState> emit,
   ) async {
-    emit(state.copyWith(isLoading: true));
+    emit(state.copyWith(isLoading: true, failureOrSuccessOption: none()));
     final response = await _service.resend(event.username);
     if (response.isLeft()) {
       emit(
@@ -90,5 +93,11 @@ class VerificationCodeBloc
         failureOrSuccessOption: some(right(response.getRightOrThrow())),
       ),
     );
+  }
+
+  @override
+  Future<void> close() async {
+    await getIt.resetLazySingleton<VerificationCodeBloc>();
+    return super.close();
   }
 }
