5a713c0be5917ee3211fc0f23a6afbc37d54ef7e
Daniel Obłąk <daniel.oblak@codeclusive.io>
2023-10-29 14:39:43 +0100

    Added Singleton decorator above AuthService, removed login method implementation and added implementation for ValidateToken method in AuthService

--- lib/auth/services/auth_service.dart
+++ lib/auth/services/auth_service.dart
@@ -2,40 +2,40 @@ import 'dart:developer';
 
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
 import 'package:kordi_mobile/auth/interfaces/auth_interface.dart';
 import 'package:kordi_mobile/core/models/kordi_exception.dart';
 import 'package:kordi_mobile/core/utils/dio_client.dart';
 import 'package:kordi_mobile/dependency_injection.dart';
 import 'package:kordi_mobile/sign_up/utils/response_code_converter.dart';
 
+@Singleton(as: AuthInterface)
 class AuthService implements AuthInterface {
   @override
-  Future<Either<KordiException, Unit>> login(
+  Future<Either<KordiException, Unit>> validateToken(
     String username,
-    String password,
+    String token,
   ) async {
-    final DioClient _dioClient = getIt.get<DioClient>();
+    log('[AuthService] validateToken()');
+    final dioClient = getIt.get<DioClient>();
     late Either<KordiException, Unit> result;
     try {
-      await _dioClient.dio.post(
-        '/login',
-        data: {
-          'username': username,
-          'password': password,
-        },
-      );
+      await dioClient.dio.post('/validate?username=$username&token=$token');
+      result = right(unit);
     } on DioException catch (e, s) {
-      log('[AuthService] login() exception: $e', stackTrace: s);
+      log('[AuthService] validateToken() DioException: $e, $s');
       if (e.response?.statusCode == 401) {
         result = left(KordiException.unauthorized());
-      } else if (e.response?.statusCode == 400) {
-        final String message =
-            ResponseCodeConverter.convert(e.response?.data['error']);
-        result = left(
-          KordiException.customMessage(message: message),
-        );
       } else {
-        result = left(KordiException.serverError());
+        if (e.response?.data is String) {
+          result =
+              left(KordiException.customMessage(message: e.response!.data));
+        } else {
+          final Map data = Map.from(e.response!.data);
+          final String message =
+              ResponseCodeConverter.convert(data.entries.first.value);
+          result = left(KordiException.customMessage(message: message));
+        }
       }
     }
     return result;
