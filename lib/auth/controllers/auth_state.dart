9f16ce4e4083afd796fe2747e5e7b7443dc5b773
Daniel Obłąk <daniel.oblak@codeclusive.io>
2023-10-29 14:38:05 +0100

    Added to AuthState.authorized param username, implemented extension isAuthorized and username in AuthState

--- lib/auth/controllers/auth_state.dart
+++ lib/auth/controllers/auth_state.dart
@@ -1,16 +1,28 @@
-part of 'auth_bloc.dart';
+part of 'auth_cubit.dart';
 
 @freezed
 class AuthState with _$AuthState {
   const factory AuthState.unauthorized() = _Unauthorized;
-  const factory AuthState.authorized(String token) = _Authorized;
+  const factory AuthState.authorized(
+    String username,
+    String token,
+  ) = Authorized;
   const factory AuthState.exception(KordiException exception) = _Exception;
   const factory AuthState.loading() = _Loading;
 }
 
 extension AuthStateEx on AuthState {
+  bool get isAuthorized => maybeMap(
+        authorized: (_) => true,
+        orElse: () => false,
+      );
+
   String get token => maybeMap(
         authorized: (state) => state.token,
+        orElse: () => '',
+      );
+  String get username => maybeMap(
+        authorized: (state) => state.username,
         orElse: () => '',
       );
 }
