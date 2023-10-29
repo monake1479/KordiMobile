3861342b6714cb873643d5814dc39b9a82084bed
Daniel Obłąk <daniel.oblak@codeclusive.io>
2023-10-29 14:41:33 +0100

    Implemented show method in KordiDialog with KordiInformationDialog

--- lib/core/utils/kordi_dialog.dart
+++ lib/core/utils/kordi_dialog.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:kordi_mobile/core/models/kordi_exception.dart';
 import 'package:kordi_mobile/core/widgets/kordi_exception_dialog.dart';
+import 'package:kordi_mobile/core/widgets/kordi_information_dialog.dart';
 
 class KordiDialog {
+  static Future<void> show(
+    BuildContext context, {
+    required String title,
+    required String subtitle,
+    required String buttonLabel,
+    required void Function() onButtonOnPressed,
+  }) async =>
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        useRootNavigator: true,
+        builder: (context) => KordiInformationDialog(
+          title: title,
+          subtitle: subtitle,
+          buttonLabel: buttonLabel,
+          onButtonOnPressed: onButtonOnPressed,
+        ),
+      );
+
   static Future<void> showException(
     BuildContext context,
     KordiException exception, {
