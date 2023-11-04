11ed037564ec237f124be792b308e1d7d2b9afee
Daniel <daniel.oblak99@gmail.com>
2023-11-04 19:29:14 +0100

    Implementation KordiTextField with optional text obscure

---
+++ lib/core/widgets/kordi_text_field.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class KordiTextField extends StatefulWidget {
+  const KordiTextField({
+    super.key,
+    required this.labelText,
+    required this.hintText,
+    required this.shouldShowErrorText,
+    required this.errorText,
+    required this.prefixIcon,
+    required this.onChanged,
+    this.obscureNeeded = false,
+  });
+  final String labelText;
+  final String hintText;
+  final bool shouldShowErrorText;
+  final String errorText;
+  final Widget prefixIcon;
+  final void Function(String) onChanged;
+  final bool obscureNeeded;
+
+  @override
+  State<KordiTextField> createState() => _KordiTextFieldState();
+}
+
+class _KordiTextFieldState extends State<KordiTextField> {
+  bool isObscure = true;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.all(8),
+        labelText: widget.labelText,
+        hintText: widget.hintText,
+        errorText: widget.shouldShowErrorText ? widget.errorText : null,
+        prefixIcon: widget.prefixIcon,
+        border: OutlineInputBorder(),
+        suffixIcon: Builder(
+          builder: (context) {
+            if (!widget.obscureNeeded) {
+              return const SizedBox();
+            }
+            return IconButton(
+              onPressed: () {
+                setState(() {
+                  isObscure = !isObscure;
+                });
+              },
+              icon: Builder(
+                builder: (context) {
+                  if (isObscure) {
+                    return Icon(
+                      Icons.visibility,
+                    );
+                  } else {
+                    return Icon(
+                      Icons.visibility_off,
+                    );
+                  }
+                },
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
