6d25fc17078a8bbeb9a1262ae58dcbfbe8ba6bfb
Daniel Obłąk <daniel.oblak@codeclusive.io>
2023-12-05 20:58:20 +0100

    Implemented KordiSearchButton

---
+++ lib/core/widgets/kordi_search_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:kordi_mobile/core/utils/kordi_routes.dart';
+import 'package:kordi_mobile/core/widgets/collections_filter_dialog.dart';
+
+class KordiSearchButton extends StatelessWidget {
+  const KordiSearchButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final colorScheme = theme.colorScheme;
+    return IconButton(
+      icon: Icon(
+        Icons.search,
+        color: colorScheme.primary,
+      ),
+      onPressed: () async {
+        final actualPath = GoRouterState.of(context).fullPath;
+        if (actualPath != CollectionPageRoute().location) {
+          CollectionPageRoute().go(context);
+        }
+        await CollectionsFilterDialog.show(context);
+      },
+    );
+  }
+}
