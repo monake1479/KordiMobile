f0948952e123da4bfa43af1ca5d06d2a886de9bc
Daniel <daniel.oblak99@gmail.com>
2023-12-17 19:08:30 +0100

    Changed how Localization is passed to KordiApp

--- lib/kordi_app.dart
+++ lib/kordi_app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:kordi_mobile/core/controllers/localization_bloc.dart';
@@ -8,6 +8,7 @@ import 'package:kordi_mobile/core/utils/custom_scroll_behavior.dart';
 import 'package:kordi_mobile/core/utils/kordi_dialog.dart';
 import 'package:kordi_mobile/core/utils/kordi_router.dart';
 import 'package:kordi_mobile/dependency_injection.dart';
+import 'package:kordi_mobile/gen/l10n.dart';
 import 'package:kordi_mobile/theme/color_schemes.dart';
 import 'package:kordi_mobile/user/controllers/get_user_cubit.dart';
 
@@ -31,8 +32,13 @@ class KordiApp extends StatelessWidget {
               }
             },
             child: MaterialApp.router(
-              localizationsDelegates: AppLocalizations.localizationsDelegates,
-              supportedLocales: AppLocalizations.supportedLocales,
+              localizationsDelegates: [
+                S.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+              ],
+              supportedLocales: S.delegate.supportedLocales,
               locale: state,
               builder: (context, child) {
                 return ScrollConfiguration(
