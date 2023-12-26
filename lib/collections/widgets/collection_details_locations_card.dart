f3843f776504ae1c8eb79247f032228ca7d1aeb9
Daniel <daniel.oblak99@gmail.com>
2023-12-26 18:44:57 +0100

    Re-implement CollectionDetailsLocationsTile

--- lib/collections/widgets/collection_details_locations_card.dart
+++ lib/collections/widgets/collection_details_locations_card.dart
@@ -1,41 +1,93 @@
-import 'package:flutter/material.dart';
-import 'package:kordi_mobile/collections/models/collections_models.dart';
-import 'package:kordi_mobile/collections/widgets/localization_tile.dart';
-import 'package:kordi_mobile/l10n/l10n.dart';
+part of 'package:kordi_mobile/collections/pages/collection_details_page.dart';
 
-class CollectionDetailsLocationsTile extends StatelessWidget {
-  const CollectionDetailsLocationsTile({super.key, required this.addresses});
+class _CollectionDetailsLocationsTile extends StatelessWidget {
+  const _CollectionDetailsLocationsTile({super.key, required this.addresses});
   final List<CollectionAddresses> addresses;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final colorScheme = theme.colorScheme;
 
     return SliverToBoxAdapter(
       child: Card(
         child: Padding(
           padding: const EdgeInsets.symmetric(
-            vertical: 8,
-            horizontal: 8,
+            vertical: 12,
+            horizontal: 12,
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Text(
-                context.l10n.collectionDetailsLocationsLabel,
-                style: theme.textTheme.bodyLarge,
-                textAlign: TextAlign.justify,
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8),
+                    child: Icon(
+                      Icons.pin_drop_sharp,
+                      color: colorScheme.primary,
+                    ),
+                  ),
+                  Text(
+                    S.current.collectionDetailsLocationsLabel,
+                    style: theme.textTheme.bodyLarge,
+                    textAlign: TextAlign.justify,
+                  ),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-                child: LocalizationTile(
-                  addresses: addresses,
-                ),
+              Builder(
+                builder: (context) {
+                  if (addresses.isEmpty) {
+                    return Text(S.current.collectionDetailsLocationsEmptyState);
+                  }
+                  return Column(
+                    children: addresses
+                        .map(
+                          (address) => Row(
+                            children: [
+                              Text('• '),
+                              Text(
+                                address.fullAddress,
+                                textAlign: TextAlign.left,
+                              ),
+                              IconButton(
+                                onPressed: () async {
+                                  await _onLocationButtonOnTap(
+                                    address,
+                                    context,
+                                  );
+                                },
+                                icon: Icon(
+                                  Icons.map_rounded,
+                                  color: colorScheme.primary,
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                        .toList(),
+                  );
+                },
               ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  Future<void> _onLocationButtonOnTap(
+    CollectionAddresses address,
+    BuildContext context,
+  ) async {
+    if (!await canLaunchUrl(address.googleMapsUrl)) {
+      await KordiDialog.showException(
+        context,
+        KordiException.customMessage(
+          message: S.current.collectionDetailsAddressesUrlException,
+        ),
+      );
+    }
+    await launchUrl(address.googleMapsUrl);
   }
 }
