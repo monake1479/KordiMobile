0b62b96ff4938a439fc6baee769a1875d6989da3
Daniel <daniel.oblak99@gmail.com>
2024-01-03 20:41:07 +0100

    Implemented EditCollectionDto

---
+++ lib/collections/models/edit_collection_dto.dart
@@ -0,0 +1,19 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:kordi_mobile/collections/models/collections_models.dart';
+
+part 'edit_collection_dto.freezed.dart';
+part 'edit_collection_dto.g.dart';
+
+@freezed
+class EditCollectionDto with _$EditCollectionDto {
+  const factory EditCollectionDto({
+    required int id,
+    required String title,
+    required String description,
+    required int userId,
+    required List<CollectionAddress> addresses,
+    required List<CollectionItem> items,
+  }) = _EditCollectionDto;
+  factory EditCollectionDto.fromJson(Map<String, dynamic> json) =>
+      _$EditCollectionDtoFromJson(json);
+}
