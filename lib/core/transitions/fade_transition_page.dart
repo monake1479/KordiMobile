ba5982b7b1a2924707db19c515cd767f075a0bd0
Daniel <daniel.oblak99@gmail.com>
2024-01-04 12:24:21 +0100

    Added missing key parameter in FadeTransitionPage

--- lib/core/transitions/fade_transition_page.dart
+++ lib/core/transitions/fade_transition_page.dart
@@ -7,6 +7,7 @@ class FadeTransitionPage extends CustomTransitionPage<void> {
     required super.child,
   }) : super(
           transitionsBuilder: (c, animation, a2, child) => FadeTransition(
+            key: key,
             opacity: animation.drive(_curveTween),
             child: child,
           ),
