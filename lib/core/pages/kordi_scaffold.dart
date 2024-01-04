096e71724c2ab3d029c4b509693445aba32c82eb
Daniel <daniel.oblak99@gmail.com>
2024-01-04 12:30:30 +0100

    Removed unnecessary MultiBlocProvider in KordiScaffold

--- lib/core/pages/kordi_scaffold.dart
+++ lib/core/pages/kordi_scaffold.dart
@@ -2,12 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:kordi_mobile/auth/controllers/auth_cubit.dart';
-import 'package:kordi_mobile/collections/controllers/collections_filter/collections_filter_bloc.dart';
-import 'package:kordi_mobile/collections/controllers/get_collections/get_collections_cubit.dart';
 import 'package:kordi_mobile/core/navigation/kordi_router.dart';
 import 'package:kordi_mobile/core/widgets/kordi_drawer.dart';
 import 'package:kordi_mobile/core/widgets/kordi_search_button.dart';
-import 'package:kordi_mobile/dependency_injection.dart';
 import 'package:kordi_mobile/gen/assets.gen.dart';
 import 'package:kordi_mobile/gen/l10n.dart';
 
@@ -19,81 +16,70 @@ class KordiScaffold extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final colorScheme = theme.colorScheme;
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<GetCollectionsCubit>(
-          create: (context) => getIt.get<GetCollectionsCubit>(),
-        ),
-        BlocProvider<CollectionsFilterBloc>(
-          create: (context) => getIt.get<CollectionsFilterBloc>()
-            ..add(CollectionsFilterEvent.getCollections()),
-        ),
-      ],
-      child: Scaffold(
-        backgroundColor: colorScheme.background,
-        appBar: AppBar(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-              bottom: Radius.circular(12),
-            ),
+    return Scaffold(
+      backgroundColor: colorScheme.background,
+      appBar: AppBar(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(12),
           ),
-          centerTitle: false,
-          leadingWidth: 30,
-          leading: Builder(
-            builder: (context) {
-              return IconButton(
-                icon: Icon(
-                  Icons.menu,
-                  color: colorScheme.primary,
-                ),
-                onPressed: () {
-                  Scaffold.of(context).openDrawer();
-                },
-                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-              );
-            },
-          ),
-          title: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 6),
-                child: GestureDetector(
-                  onTap: () => CollectionPageRoute().go(context),
-                  child: Assets.icons.logo.image(
-                    width: 38,
-                  ),
-                ),
-              ),
-              Text(
-                S.current.kordiScaffoldTitle,
-                style: TextStyle(color: colorScheme.secondary),
-              ),
-            ],
-          ),
-          actions: [
-            KordiSearchButton(),
-          ],
         ),
-        drawer: KordiDrawer(),
-        floatingActionButton: Builder(
+        centerTitle: false,
+        leadingWidth: 30,
+        leading: Builder(
           builder: (context) {
-            final authState = context.read<AuthCubit>().state;
-            if (authState is Authorized &&
-                GoRouterState.of(context).fullPath == '/collection') {
-              return FloatingActionButton(
-                child: Icon(Icons.add),
-                onPressed: () {
-                  CreateCollectionFirstStepRoute().go(
-                    context,
-                  );
-                },
-              );
-            }
-            return const SizedBox();
+            return IconButton(
+              icon: Icon(
+                Icons.menu,
+                color: colorScheme.primary,
+              ),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
           },
         ),
-        body: child,
+        title: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 6),
+              child: GestureDetector(
+                onTap: () => CollectionPageRoute().go(context),
+                child: Assets.icons.logo.image(
+                  width: 38,
+                ),
+              ),
+            ),
+            Text(
+              S.current.kordiScaffoldTitle,
+              style: TextStyle(color: colorScheme.secondary),
+            ),
+          ],
+        ),
+        actions: [
+          KordiSearchButton(),
+        ],
       ),
+      drawer: KordiDrawer(),
+      floatingActionButton: Builder(
+        builder: (context) {
+          final authState = context.read<AuthCubit>().state;
+          if (authState is Authorized &&
+              GoRouterState.of(context).fullPath == '/collection') {
+            return FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () {
+                CreateCollectionFirstStepRoute().go(
+                  context,
+                );
+              },
+            );
+          }
+          return const SizedBox();
+        },
+      ),
+      body: child,
     );
   }
 }
