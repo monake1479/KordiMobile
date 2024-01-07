6eab0ae61b98d43b15f84a081d36fb745dafaee7
Daniel <daniel.oblak99@gmail.com>
2024-01-07 12:06:53 +0100

    Removed ManageCollectionAddressCubit provider in CollectionDetailsPage

--- lib/collections/pages/collection_details_page.dart
+++ lib/collections/pages/collection_details_page.dart
@@ -36,6 +36,7 @@ class CollectionDetailsPage extends StatelessWidget {
     final userId = context.read<GetUserCubit>().state.user?.id;
     final collectionIdString =
         GoRouterState.of(context).pathParameters['collectionId'];
+
     if (collectionIdString == null) {
       KordiDialog.showException(
         context,
@@ -54,10 +55,6 @@ class CollectionDetailsPage extends StatelessWidget {
             ..add(
               EditCollectionFormEvent.setInitial(collectionId),
             ),
-        ),
-        BlocProvider<ManageCollectionAddressCubit>(
-          create: (context) => getIt.get<ManageCollectionAddressCubit>()
-            ..setAddresses(collectionId),
         ),
       ],
       child: BlocBuilder<EditCollectionFormBloc, EditCollectionFormState>(
