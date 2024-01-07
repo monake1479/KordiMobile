2bf5c11263680d76e92da686969545bc2aa13f16
Daniel <daniel.oblak99@gmail.com>
2024-01-07 16:53:13 +0100

    Removed comment export from collections models barrel file

--- lib/collections/models/collections_models.dart
+++ lib/collections/models/collections_models.dart
@@ -4,5 +4,4 @@ export 'package:kordi_mobile/collections/models/collection_dto.dart';
 export 'package:kordi_mobile/collections/models/collection_filter.dart';
 export 'package:kordi_mobile/collections/models/collection_paging.dart';
 export 'package:kordi_mobile/collections/models/collection_status.dart';
-export 'package:kordi_mobile/collections/models/comment.dart';
 export 'package:kordi_mobile/collections/models/edit_collection_dto.dart';
