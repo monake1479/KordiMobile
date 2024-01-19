009299cdc32c7e413ef228c6a7b5af794ba194ee
Daniel <daniel.oblak99@gmail.com>
2024-01-19 20:01:33 +0100

    Removed unused image parameter from EditCollectionDto

--- lib/collections/models/edit_collection_dto.dart
+++ lib/collections/models/edit_collection_dto.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:kordi_mobile/core/utils/base_to_string_converter.dart';
 
 part 'edit_collection_dto.freezed.dart';
 part 'edit_collection_dto.g.dart';
@@ -11,7 +10,6 @@ class EditCollectionDto with _$EditCollectionDto {
     required int id,
     required String title,
     required String description,
-    @BaseToStringConverter() required Uint8List? image,
     required int userId,
   }) = _EditCollectionDto;
   factory EditCollectionDto.fromJson(Map<String, dynamic> json) =>
