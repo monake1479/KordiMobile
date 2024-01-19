5906997dfe022a4470550d110b94497c924facdb
Daniel <daniel.oblak99@gmail.com>
2024-01-19 19:59:19 +0100

    Replaced context.read of authState to BlocBuilder of AuthCubit in DonationSlider

--- lib/collection_items/widgets/donation_slider.dart
+++ lib/collection_items/widgets/donation_slider.dart
@@ -11,7 +11,6 @@ class _DonationSlider extends StatelessWidget {
     final theme = Theme.of(context);
     final colorScheme = theme.colorScheme;
     final textTheme = theme.textTheme;
-    final authState = context.watch<AuthCubit>().state;
     final collectionIdString =
         GoRouterState.of(context).pathParameters['collectionId'];
     final collectionId = int.parse(collectionIdString!);
@@ -38,91 +37,95 @@ class _DonationSlider extends StatelessWidget {
             );
           }
         },
-        child: BlocBuilder<DonationFormBloc, DonationFormState>(
-          builder: (context, state) {
-            final manageCollectionItemsCubit =
-                context.read<ManageCollectionItemsCubit>();
-            final donationFormBloc = context.read<DonationFormBloc>();
-            if (item.isFinished) {
-              return Text(
-                S.current.collectionDetailsItemCompletedLabel,
-                textAlign: TextAlign.center,
-                style: textTheme.titleMedium!.copyWith(
-                  color: colorScheme.primary,
-                  fontWeight: FontWeight.bold,
-                ),
-              );
-            }
-            if (!authState.isAuthorized) {
-              return const SizedBox.shrink();
-            }
-            return Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Row(
-                    children: [
-                      Text(
-                        S.current.collectionDetailsItemDonateLabel,
-                      ),
-                      Text(
-                        state.amountString(
-                          item.currentAmount!,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Slider(
-                    value: item.currentAmount != null &&
-                            (item.currentAmount! > state.amount)
-                        ? item.currentAmount?.toDouble() ?? 0
-                        : state.amount.toDouble(),
-                    max: item.maxAmount.toDouble(),
-                    label: state.amountString(
-                      item.currentAmount!,
+        child: BlocBuilder<AuthCubit, AuthState>(
+          builder: (context, authState) {
+            return BlocBuilder<DonationFormBloc, DonationFormState>(
+              builder: (context, state) {
+                final manageCollectionItemsCubit =
+                    context.read<ManageCollectionItemsCubit>();
+                final donationFormBloc = context.read<DonationFormBloc>();
+                if (item.isFinished) {
+                  return Text(
+                    S.current.collectionDetailsItemCompletedLabel,
+                    textAlign: TextAlign.center,
+                    style: textTheme.titleMedium!.copyWith(
+                      color: colorScheme.primary,
+                      fontWeight: FontWeight.bold,
                     ),
-                    divisions: item.maxAmount,
-                    onChanged: (amount) {
-                      donationFormBloc.add(
-                        DonationFormEvent.setAmount(
-                          amount.toInt(),
+                  );
+                }
+                if (!authState.isAuthorized) {
+                  return const SizedBox.shrink();
+                }
+                return Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 8),
+                      child: Row(
+                        children: [
+                          Text(
+                            S.current.collectionDetailsItemDonateLabel,
+                          ),
+                          Text(
+                            state.amountString(
+                              item.currentAmount!,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 8),
+                      child: Slider(
+                        value: item.currentAmount != null &&
+                                (item.currentAmount! > state.amount)
+                            ? item.currentAmount?.toDouble() ?? 0
+                            : state.amount.toDouble(),
+                        max: item.maxAmount.toDouble(),
+                        label: state.amountString(
                           item.currentAmount!,
                         ),
-                      );
-                    },
-                  ),
-                ),
-                Builder(
-                  builder: (context) {
-                    if (state.amount == 0 ||
-                        state.fixedAmount(item.currentAmount!) == 0) {
-                      return const SizedBox.shrink();
-                    }
-
-                    return ElevatedButton(
-                      onPressed: () async {
-                        await manageCollectionItemsCubit.donate(
-                          state.toDto(item.currentAmount!),
-                          collectionId,
-                        );
-                        context.read<CollectionsFilterBloc>().add(
-                              CollectionsFilterEvent.getById(
-                                collectionId,
-                              ),
-                            );
-                        manageCollectionItemsCubit
-                            .updateDonation(state.toDto(item.currentAmount!));
-                      },
-                      child: Text(
-                        S.current.collectionDetailsItemDonateButtonLabel,
+                        divisions: item.maxAmount,
+                        onChanged: (amount) {
+                          donationFormBloc.add(
+                            DonationFormEvent.setAmount(
+                              amount.toInt(),
+                              item.currentAmount!,
+                            ),
+                          );
+                        },
                       ),
-                    );
-                  },
-                ),
-              ],
+                    ),
+                    Builder(
+                      builder: (context) {
+                        if (state.amount == 0 ||
+                            state.fixedAmount(item.currentAmount!) == 0) {
+                          return const SizedBox.shrink();
+                        }
+
+                        return ElevatedButton(
+                          onPressed: () async {
+                            await manageCollectionItemsCubit.donate(
+                              state.toDto(item.currentAmount!),
+                              collectionId,
+                            );
+                            context.read<CollectionsFilterBloc>().add(
+                                  CollectionsFilterEvent.getById(
+                                    collectionId,
+                                  ),
+                                );
+                            manageCollectionItemsCubit.updateDonation(
+                                state.toDto(item.currentAmount!));
+                          },
+                          child: Text(
+                            S.current.collectionDetailsItemDonateButtonLabel,
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                );
+              },
             );
           },
         ),
