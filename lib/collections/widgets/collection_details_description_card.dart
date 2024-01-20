1873c7785203601b5e8ea3c198ef02f6ab375705
Daniel <daniel.oblak99@gmail.com>
2024-01-20 14:41:56 +0100

    Removed description parameter from CollectionDetailsDescriptionCard and replaced it with BlocBuilder

--- lib/collections/widgets/collection_details_description_card.dart
+++ lib/collections/widgets/collection_details_description_card.dart
@@ -1,42 +1,43 @@
 part of 'package:kordi_mobile/collections/pages/collection_details_page.dart';
 
 class _CollectionDetailsDescriptionCard extends StatelessWidget {
-  const _CollectionDetailsDescriptionCard({
-    required this.description,
-  });
-  final String description;
+  const _CollectionDetailsDescriptionCard();
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return SliverToBoxAdapter(
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            vertical: 12,
-            horizontal: 12,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                S.current.collectionDetailsDescriptionLabel,
-                style: theme.textTheme.bodyLarge,
-                textAlign: TextAlign.justify,
+    return BlocBuilder<EditCollectionFormBloc, EditCollectionFormState>(
+      builder: (context, state) {
+        return SliverToBoxAdapter(
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 12,
+                horizontal: 12,
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-                child: Text(
-                  description,
-                  style: theme.textTheme.bodyLarge,
-                  textAlign: TextAlign.justify,
-                ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    S.current.collectionDetailsDescriptionLabel,
+                    style: theme.textTheme.bodyLarge,
+                    textAlign: TextAlign.justify,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: Text(
+                      state.description,
+                      style: theme.textTheme.bodyLarge,
+                      textAlign: TextAlign.justify,
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
