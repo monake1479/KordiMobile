c7ef92f081053f99ee534445044cc4334de3c323
Daniel <daniel.oblak99@gmail.com>
2024-02-11 13:02:08 +0100

    Removed itemCurrentAmount parameter, added CollectionItem parameter in setAmount method, added resetAmount event in DonationFormEvent

--- lib/collection_items/controllers/donation_form/donation_form_event.dart
+++ lib/collection_items/controllers/donation_form/donation_form_event.dart
@@ -2,12 +2,13 @@ part of 'package:kordi_mobile/collection_items/controllers/donation_form/donatio
 
 @freezed
 class DonationFormEvent with _$DonationFormEvent {
+  const factory DonationFormEvent.resetAmount() = _ResetAmount;
   const factory DonationFormEvent.setInitialState(
     CollectionItem collectionItem,
     int collectionId,
   ) = _SetInitialState;
   const factory DonationFormEvent.setAmount(
     int amount,
-    int itemCurrentAmount,
+    CollectionItem item,
   ) = _SetAmount;
 }
