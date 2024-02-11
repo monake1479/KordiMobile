10b78bcec29f92e2211468bf212d405b918f3e1a
Daniel <daniel.oblak99@gmail.com>
2024-02-11 16:32:48 +0100

    Removed BlocBuilder of EditCollectionFormBloc in CollectionDetailsDescriptionCard

--- lib/collections/widgets/collection_details_description_card.dart
+++ lib/collections/widgets/collection_details_description_card.dart
@@ -1,43 +1,39 @@
 part of 'package:kordi_mobile/collections/pages/collection_details_page.dart';
 
 class _CollectionDetailsDescriptionCard extends StatelessWidget {
-  const _CollectionDetailsDescriptionCard();
-
+  const _CollectionDetailsDescriptionCard(this.description);
+  final String description;
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return BlocBuilder<EditCollectionFormBloc, EditCollectionFormState>(
-      builder: (context, state) {
-        return SliverToBoxAdapter(
-          child: Card(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 12,
-                horizontal: 12,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    S.current.collectionDetailsDescriptionLabel,
-                    style: theme.textTheme.bodyLarge,
-                    textAlign: TextAlign.justify,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    child: Text(
-                      state.description,
-                      style: theme.textTheme.bodyLarge,
-                      textAlign: TextAlign.justify,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+    return SliverToBoxAdapter(
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 12,
+            horizontal: 12,
           ),
-        );
-      },
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                S.current.collectionDetailsDescriptionLabel,
+                style: theme.textTheme.bodyLarge,
+                textAlign: TextAlign.justify,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Text(
+                  description,
+                  style: theme.textTheme.bodyLarge,
+                  textAlign: TextAlign.justify,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
